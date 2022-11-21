603b0bed48732d83d4402eee5c3b90a032f53c5c
Daniel Diaz <danieldiazgonza52@gmail.com>
2022-11-20 21:04:28 -0500

    Added Calendar Feature

--- habit/admin.py
+++ habit/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from .models import Habit, Daily
+
+
+class DailyAdmin(admin.ModelAdmin):
+    list_display = ('habit', 'date', 'is_active')
 # Register your models here.
 admin.site.register(Habit)
-admin.site.register(Daily)
+admin.site.register(Daily, DailyAdmin)
